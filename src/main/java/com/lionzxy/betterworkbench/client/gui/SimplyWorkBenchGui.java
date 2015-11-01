a147fe1ce4ed5bd7ef86e3080d33765a7e5e279f
LionZXY <nikita@kulikof.ru>
2015-11-01 22:56:08 +0300

    Start Add custom GUI

--- src/main/java/com/lionzxy/betterworkbench/client/gui/SimplyWorkBenchGui.java
+++ src/main/java/com/lionzxy/betterworkbench/client/gui/SimplyWorkBenchGui.java
@@ -25,6 +25,15 @@ import org.lwjgl.opengl.GL11;
  */
 public class SimplyWorkBenchGui extends GuiContainer {
 
+    //176 72 : 200 107 - Боковущка слева сверху 25 : 36
+    //202 72 : 226 107 - Боковушка справа сверху 25 : 36
+    //176 108 : 200 132 - Боковушка слева снизу
+    //202 108 : 226 132 - Боковушка справа снизу
+    //176 0 : 193 35 - Палочка вверх 18 : 36
+    //194 0 : 211 23 - Палочка вниз
+    //176 36 : 200 53 - Палочка влево 25 18
+    //176 54 : 200 71 - Палочка вправо 25 18
+    //202 36 : 219 53 - Слот 17
     protected ResourceLocation background = new ResourceLocation(Constant.MODID, "textures/gui/crafting_table.png");
 
     public SimplyWorkBenchGui(IInventory workBenchInventory, EntityPlayer player, ItemStack[] itemList) {
@@ -42,6 +51,56 @@ public class SimplyWorkBenchGui extends GuiContainer {
     protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         mc.renderEngine.bindTexture(background);
+        //System.out.println("Left: " + guiLeft + " Top: " + guiTop);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, 176, 166);
+        generate(20);
     }
+
+    public void generate(int x) {
+        int columns = ((guiLeft - 48) / 18);
+        if (x < (columns + 2))
+            columns = x - 2;
+        int row = x / columns;
+        int startX = guiLeft - 50 - columns * 18;
+        int thisX = startX;
+        int thisY = guiTop;
+        drawTexturedModalRect(thisX, guiTop, 176, 72, 25, 36);
+        thisX += 25;
+        for (int i = 0; i < columns; i++) {
+            drawTexturedModalRect(thisX, guiTop, 176, 0, 18, 36);
+            thisX += 18;
+        }
+        drawTexturedModalRect(thisX, guiTop, 202, 72, 24, 36);
+        thisX = startX;
+        for (int r = 2; r < (row - 1); r++) {
+            //System.out.println("X: " + thisX + " Y: " + thisY);
+            thisY += 18;
+            drawTexturedModalRect(thisX, thisY, 176, 36, 25, 18);
+            thisX += 25;
+            for (int i = 0; i < columns; i++) {
+                drawTexturedModalRect(thisX, thisY, 194, 0, 18, 36);
+                thisX += 18;
+            }
+            drawTexturedModalRect(thisX, thisY, 176, 108, 25, 36);
+            thisX = startX;
+        }
+        thisY += 18;
+        //System.out.println(startX);
+        int lastColumn;
+        if (x > (columns + 2))
+            lastColumn = x - (columns + 2) * (row);
+        else lastColumn = columns;
+        drawTexturedModalRect(thisX, thisY, 202, 108, 25, 36);
+        if (lastColumn > 0)
+            thisX += 25;
+        else thisX += 7;
+        for (int i = 0; i < lastColumn; i++) {
+            drawTexturedModalRect(thisX, thisY, 194, 0, 18, 36);
+            thisX += 18;
+        }
+        drawTexturedModalRect(thisX, thisY, 176, 108, 25, 36);
+        thisX = startX;
+
+    }
+
 }
